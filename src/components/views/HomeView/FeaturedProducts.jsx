3cffb9fe83b297cfd8fb56f6f918c667dfb95735
FazeElian <elian.ibarra@soy.sena.edu.co>
2023-12-31 20:20:37 -0500

    Sección Productos Destacados

---
+++ src/components/views/HomeView/FeaturedProducts.jsx
@@ -0,0 +1,71 @@
+// Navegación entre rutas
+import { Link } from "react-router-dom";
+
+// Estilos para este componente
+import "../../../assets/css/components/HomeView/FeaturedProducts.css";
+
+// Imágenes - Iconos
+    // Producto Destacado 1
+    import featuredProductImage1 from "../../../assets/img/featuredProducts/featuredProductImage1.jpg";
+
+    // Producto Destacado 2
+    import featuredProductImage2 from "../../../assets/img/featuredProducts/featuredProductImage2.jpg";
+
+    // Producto Destacado 3
+    import featuredProductImage3 from "../../../assets/img/featuredProducts/featuredProductImage3.jpg";
+
+    // Producto Destacado 4
+    import featuredProductImage4 from "../../../assets/img/featuredProducts/featuredProductImage4.jpg";
+
+const FeaturedProducts = () => {
+    // Lista de Productos Destacados
+    const ListFeaturedProducts = [
+        {
+            id: 1,
+            image: featuredProductImage1,
+            title: "Buque ARC Barranquilla",
+            description: "Descripción Producto 1"
+        },
+        {
+            id: 2,
+            image: featuredProductImage2,
+            title: "Cuadro Marinero Vintage",
+            description: "Descripción Producto 2"
+        },
+        {
+            id: 3,
+            image: featuredProductImage3,
+            title: "Bote Marinero Infantería de Marina",
+            description: "Descripción Producto 3"
+        },
+        {
+            id: 4,
+            image: featuredProductImage4,
+            title: "Cuadro Marinero Simple",
+            description: "Descripción Producto 4"
+        },
+    ];
+
+    return(
+        <div className="featured-prods">
+            <div className="cont-title-section">
+                <div className="title-featured-prods">Productos más destacados</div>
+            </div>
+            <div className="cont-featured-prods">
+                {ListFeaturedProducts.map((product) => (
+                    <div className="item-featured-prod" key={product.id}>
+                        <div className="cont-img-item-featured-prod">
+                            <img src={product.image} alt="" />
+                        </div>
+                        <div className="cont-text-item-featured-prod">
+                            <h2><Link>{product.title}</Link></h2>
+                            <p>{product.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export { FeaturedProducts };
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -4,6 +4,9 @@
 
     // Banner de Bote
     import { BoatBanner } from "../components/views/HomeView/BoatBanner";
+
+    // Productos más destacados
+    import { FeaturedProducts } from "../components/views/HomeView/FeaturedProducts";
         
 const HomeView = () => {
     return(
@@ -13,6 +16,9 @@ const HomeView = () => {
 
             {/* Componente Banner Bote */}
             <BoatBanner />
+
+            {/* Componente Productos más destacados */}
+            <FeaturedProducts />
         </>
     )
 }
